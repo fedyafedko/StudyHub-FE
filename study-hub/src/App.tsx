5c0d0cb784919421547590f371a20a6d2d41b837
Федько Влад <vladfedko35@gmail.com>
2024-03-23 14:18:56 +0200

    feat: create solution

---
+++ study-hub/src/App.tsx
@@ -0,0 +1,50 @@
+import './App.css';
+import SignInPage from './pages/SignInPage/SignInPage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import HomePage from './pages/HomePage';
+import { SnackbarProvider } from 'notistack';
+import ForgotPasswordPage from './pages/ForgotPasswordPage/ForgotPasswordPage';
+import DashboardPage from './pages/DashboardPage/DashboardPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#D41A6D',
+    },
+    background: {
+      default: '#f0f0f0',
+    },
+  },
+  typography: { 'fontFamily': '"Source Sans 3", sans-serif' },
+});
+
+function App() {
+  return (
+    <SnackbarProvider>
+      <ThemeProvider theme={theme}>
+        <GoogleOAuthProvider
+          clientId="738777886358-745pr29p66qlearc5r7mc7lve57lebcn.apps.googleusercontent.com"
+        >
+          <CssBaseline />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/reset-password/:email/:token" element={<ForgotPasswordPage />} />
+              <Route path="/sign-up/:token" element={<SignUpPage />} />
+              <Route path="/sign-in" element={<SignInPage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </BrowserRouter>
+        </GoogleOAuthProvider>
+      </ThemeProvider>
+    </SnackbarProvider>
+  );
+}
+
+export default App;
